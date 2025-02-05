9ccc17c89fc3110d8f609946463e5c8b921d576f
jee-in <chris309804@gmail.com>
2025-02-05 05:00:40 +0000

    feat: Update tiny server

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -17,37 +17,37 @@ void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
-/* open a listeng socket, and then repeatedly accept a connection request, perform a transation, close end of the connection in a loop*/
+/* Open a listeng socket, and then repeatedly accept a connection request, 
+   perform a transation, close its end of the connection*/
 int main(int argc, char **argv) {
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
 
-  /* Check command-line args */
   if (argc != 2) {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
 
-  listenfd = Open_listenfd(argv[1]); // opening a listening socket
+  listenfd = Open_listenfd(argv[1]);
   while (1) {
     clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // acceptig a connection request
-    Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); // converting socketaddr to form that is available to read
-    doit(connfd); // performing a transaction
-    Close(connfd); // closing its end of the connenction
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    doit(connfd); // perform a transaction
+    Close(connfd);
   }
 }
 
-/* handle one HTTP transaction. read and parse the request line and figure out whether it is static or dynamic content */
+/* Handle one HTTP transaction (only GET method is supported). 
+   Read and parse the request line, serve static or dynamic content */
 void doit(int fd) {
-
   int is_static;
   struct stat sbuf;
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   char filename[MAXLINE], cgiargs[MAXLINE];
-  rio_t rio; // Robust I/O: 버퍼링된 입출력 처리
+  rio_t rio;
 
   /* Read Request line and headers */
   Rio_readinitb(&rio, fd);
@@ -69,28 +69,28 @@ void doit(int fd) {
     return;
   }
 
-  if (is_static) {
+  if (is_static) {  /* Serve static content */
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
       clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
       return;
     }
     serve_static(fd, filename, sbuf.st_size);
-  } else {
-    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+  } else {          /* Serve dynamic content */
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
       return;
     }
     serve_dynamic(fd, filename, cgiargs);
   }
 }
 
-// send an HTTP response to the client with the appropriate status code and status message
+/* Send an HTTP response explaining the error to the browser's user */
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
   char buf[MAXLINE], body[MAXBUF];
 
   /* Build the HTTP response body */
   sprintf(body, "<html><title>Tiny Error</title>");
-  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s<body bgcolor=\"ffffff\">\r\n", body);
   sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
   sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
   sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
@@ -105,7 +105,7 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
   Rio_writen(fd, body, strlen(body));
 }
 
-/* Read and ignore request headers */
+/* Read request headers */
 void read_requesthdrs(rio_t *rp) {
   char buf[MAXLINE];
 
@@ -117,39 +117,39 @@ void read_requesthdrs(rio_t *rp) {
   return;
 }
 
-/* Parse an HHTP URI */
+/* Parse an HTTP URI */
 int parse_uri(char *uri, char *filename, char *cgiargs) {
   char *ptr;
 
-  if (!strstr(uri, "cgi-bin")) { // static content
-    strcpy(cgiargs, ""); // clear the CGI argument string
-    strcpy(filename, "."); 
-    strcat(filename, uri); // convert the URI into a relative Linux pathname such as ./index.html
-    if (uri[strlen(uri)-1] == '/') // if the URL ends with '/'
-      strcat(filename, "home.html"); // append the default file name
+  if (!strstr(uri, "cgi-bin")) { /* If the request is for static content */
+    strcpy(cgiargs, "");
+    strcpy(filename, ".");
+    strcat(filename, uri);
+    if (uri[strlen(uri)-1] == '/')
+      strcat(filename, "home.html");
     return 1;
-  } else { // dynamic content
-    ptr = index(uri, "?"); // extract any CGI arguments
+  } else {                       /* If the request is for dynamic content */
+    ptr = index(uri, '?');
     if (ptr) {
-      strcpy(cgiargs, ptr+1); 
+      strcpy(cgiargs, ptr+1);
       *ptr = '\0';
     }
     else 
       strcpy(cgiargs, "");
-    strcpy(filename, "."); // convert the remaining portion of the URI to a relative Linux filename
+    strcpy(filename, ".");
     strcat(filename, uri); 
     return 0;
   }
 }
 
-/* serve static content to a client */
+/* Serve static content to a client */
 void serve_static(int fd, char *filename, int filesize) {
   int srcfd;
   char *srcp, filetype[MAXLINE], buf[MAXBUF];
 
   /* send response headers to client */
   get_filetype(filename, filetype);
-  sprintf(buf, "HTTP/1.0 200 OK\r\n", buf);
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
   sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
   sprintf(buf, "%sConnection: close\r\n", buf);
   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
@@ -158,7 +158,7 @@ void serve_static(int fd, char *filename, int filesize) {
   printf("Response headers:\n");
   printf("%s", buf);
 
-  /* send response body to client */
+  /* Send response body to client */
   srcfd = Open(filename, O_RDONLY, 0);
   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
   Close(srcfd);
@@ -166,21 +166,21 @@ void serve_static(int fd, char *filename, int filesize) {
   Munmap(srcp, filesize);
 }
 
-/* derive file type from filename */
+/* Derive file type from filename */
 void get_filetype(char *filename, char *filetype) {
   if (strstr(filename, ".html"))
-    strcpy(filetype, "text.html");
+    strcpy(filetype, "text/html");
   else if (strstr(filename, ".gif"))
     strcpy(filetype, "image/gif");
   else if (strstr(filename, ".png"))
     strcpy(filetype, "image/png");
   else if (strstr(filename, ".jpg"))
     strcpy(filetype, "image/jpeg");
-  else 
-    strcpy(filetype, "image/png");
+  else
+    strcpy(filetype, "text/plain");
 }
 
-/* serve dynamic content to a client */
+/* Serve dynamic content to a client */
 void serve_dynamic(int fd, char *filename, char *cgiargs) {
   char buf[MAXLINE], *emptylist[] = { NULL };
 
@@ -192,9 +192,9 @@ void serve_dynamic(int fd, char *filename, char *cgiargs) {
 
   if (Fork() == 0) { /* Child */
     /* Real server would set all CGI vars here */
-    setenv("QUERY_STRING", cgiargs, 1); // the child initializes the Q_S environment variable with the CGI arguments from the request URI
-    Dup2(fd, STDOUT_FILENO); // redirect stdout to client
-    Execve(filename, emptylist, environ); // load and run the CGI program (in the context of the child)
+    setenv("QUERY_STRING", cgiargs, 1); 
+    Dup2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
+    Execve(filename, emptylist, environ); /* Load and run the CGI program */
   }
-  Wait(NULL); // parent waits for and reaps child when it terminates
+  Wait(NULL); /* Parent waits for and reaps child */
 }
